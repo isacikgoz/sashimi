6815e6e23bd6a41b886a2c10929abbfd95f3a922
İbrahim Serdar Açıkgöz <serdaracikgoz86@gmail.com>
2018-09-07 23:25:30 +0300

    initial commit.

---
+++ fish_prompt.fish
@@ -0,0 +1,40 @@
+# Defined in /home/isacikgoz/.config/fish/functions/fish_prompt.fish @ line 10
+function fish_prompt
+  set -l last_status $status
+  set -l cyan (set_color -o cyan)
+  set -l yellow (set_color -o yellow)
+  set -l red (set_color -o red)
+  set -l blue (set_color -o blue)
+  set -l green (set_color -o green)
+  set -l normal (set_color normal)
+
+  if test $last_status = 0
+    set status_indicator "$green✔︎ "
+  else
+    set status_indicator "$red✗ "
+  end
+  set -l cwd $cyan(basename (prompt_pwd))
+
+  if [ (_git_branch_name) ]
+
+    if test (_git_branch_name) = 'master'
+      set -l git_branch (_git_branch_name)
+      set git_info "$normal git:($red$git_branch$normal)"
+    else
+      set -l git_branch (_git_branch_name)
+      set git_info "$normal git:($blue$git_branch$normal)"
+    end
+
+    if [ (_is_git_dirty) ]
+      set -l dirty "$yellow ✗"
+      set git_info "$git_info$dirty"
+    end
+  end
+
+  # Notify if a command took more than 5 minutes
+  if [ "$CMD_DURATION" -gt 300000 ]
+    echo The last command took (math "$CMD_DURATION/1000") seconds.
+  end
+
+  echo -n -s $status_indicator $cwd $git_info $normal ' '(set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+end
